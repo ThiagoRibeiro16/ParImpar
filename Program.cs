3ea49ac366115e4df1379aecdcd4f44b6494e750
Thiago Ribeiro <thiago.escola321@gmail.com>
2025-05-18 16:58:04 -0300

    finaliza programa

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+double num;
+double resultado;
+
+Console.Write("Digite um número: ");
+num = Convert.ToDouble(Console.ReadLine());
+
+resultado = num % 2;
+
+if (resultado != 0)
+{
+    Console.WriteLine("o número é impar");
+}
+
+else
+{
+    Console.WriteLine("o número é par");
+
+}
